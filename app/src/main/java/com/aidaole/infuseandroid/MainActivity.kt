1b53c5a6e0de7d72b942e588b34364d1f34145e9
aidaole <aidaole@outlook.com>
2025-03-27 21:20:45 +0800

    [移除所有代码]

--- app/src/main/java/com/aidaole/infuseandroid/MainActivity.kt
+++ app/src/main/java/com/aidaole/infuseandroid/MainActivity.kt
@@ -3,17 +3,10 @@ package com.aidaole.infuseandroid
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import androidx.navigation.compose.rememberNavController
-import com.aidaole.infuseandroid.ui.components.InfuseBottomNavigation
-import com.aidaole.infuseandroid.ui.navigation.InfuseNavHost
 import com.aidaole.infuseandroid.ui.theme.InfuseAndroidTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -35,18 +28,5 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun InfuseApp() {
-    val navController = rememberNavController()
-    
-    Box(
-        modifier = Modifier.fillMaxSize()
-    ) {
-        InfuseNavHost(navController = navController)
-        
-        InfuseBottomNavigation(
-            modifier = Modifier
-                .align(Alignment.BottomCenter)
-                .padding(bottom = 8.dp),
-            navController = navController
-        )
-    }
+
 }
